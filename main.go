d6b417f75b7fb0df028c5c6fd6207eae0cac46dd
codezera <codezera3@gmail.com>
2024-07-27 18:22:17 +0530

    added validate chirp handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,22 +22,23 @@ func main() {
 
 	mux.Handle("/app/*",	apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer((http.Dir("."))))))
 
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerCheckHits)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCheckHits)
+	mux.HandleFunc("/api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileServerHits int
 }
 
 func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-type apiConfig struct {
-	fileServerHits int
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -47,13 +49,75 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerCheckHits(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Println(cfg.fileServerHits)
-	resp := fmt.Sprintf("Hits: %v", cfg.fileServerHits)
-	w.Write([]byte(resp))
+	w.Header().Set("Content-Type", "text/html")
+	// resp := fmt.Sprintf("Hits: %v", cfg.fileServerHits)
+	html := fmt.Sprintf(`<html>
+		<body>
+    	<h1>Welcome, Chirpy Admin</h1>
+    	<p>Chirpy has been visited %v times!</p>
+		</body>
+		</html>`, cfg.fileServerHits)
+
+		w.Write([]byte(html))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	cfg.fileServerHits = 0
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+
+	type errorType struct {
+		Error string `json:"error"`
+	}
+
+	type respType struct {
+		Valid bool `json:"valid"`
+	}
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		
+		err := errorType{
+			Error: "Something went wrong",
+		}
+
+		dat, _ := json.Marshal(err)
+
+		w.Write(dat)
+		w.WriteHeader(500)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		err := errorType{
+			Error: "Chirp is too long",
+		}
+
+		dat, _ := json.Marshal(err)
+
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+
+	resp := respType{
+		Valid: true,
+	}
+
+	data, _ := json.Marshal(resp)
+
+	w.WriteHeader(200)
+	w.Write(data)
 }
