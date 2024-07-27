ff08154df1d0bf8314172216e20f519e24dabde8
codezera <codezera3@gmail.com>
2024-07-27 17:23:21 +0530

    added metrics middleware and serverhit check logic

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
 
 func main() {
 	const port = "8080"
@@ -14,9 +16,14 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/*",	apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer((http.Dir("."))))))
 
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerCheckHits)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
@@ -26,4 +33,27 @@ func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerCheckHits(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Println(cfg.fileServerHits)
+	resp := fmt.Sprintf("Hits: %v", cfg.fileServerHits)
+	w.Write([]byte(resp))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileServerHits = 0
 }
